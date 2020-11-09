5b72a4c6c2cbe7a70a4aee1e8927a928c13e5a3e
Ramon Audain <raudain123@gmail.com>
2020-11-09 15:37:36 -0800

    Convert all components to classes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 // Props from TodoList.js
-const Todo = props => {
-  return (
-    <div
-      // Changes class name when item is clicked.  toggleItem function in App.js
-      className={`item${props.item.isDone ? "Completed" : ""}`}
-      onClick={() => props.toggleItem(props.item.id)}
-    >
-      <p>{props.item.task}</p>
-    </div>
-  );
-};
+class Todo extends React.Component {
+  render() {
+    console.log(this)
+    return (
+      <div
+        // Changes class name when item is clicked.  toggleItem function in App.js
+        className={`item${this.props.item.isDone ? "Completed" : ""}`}
+        onClick={() => this.props.toggleItem(this.props.item.id)}
+      >
+        <p>{this.props.item.task}</p>
+      </div>
+    );
+  };
+}
+
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,19 +2,21 @@ import React from "react";
 import Todo from "./Todo";
 
 // Getting props from App.js
-const TodoList = props => {
-  return (
-    <div className="todolist">
-      {props.todos.map(item => (
-        <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
-      ))}
-
-      {/* clearCompleted function in App.js */}
-      <button className="clearBtn" onClick={props.clearCompleted}>
-        Clear Completed
-      </button>
-    </div>
-  );
+class TodoList extends React.Component {
+  render(){
+    return (
+      <div className="todolist">
+        {this.props.todos.map(item => (
+          <Todo key={item.id} item={item} toggleItem={this.props.toggleItem} />
+        ))}
+  
+        {/* clearCompleted function in App.js */}
+        <button className="clearBtn" onClick={this.props.clearCompleted}>
+          Clear Completed
+        </button>
+      </div>
+    );
+  }
 };
 
 export default TodoList;
