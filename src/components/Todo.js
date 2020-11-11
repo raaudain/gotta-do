dbb9e478524764b29780e1e0e77410860e4c3f30
Ramon Audain <raudain123@gmail.com>
2020-11-10 18:18:42 -0800

    Working on drag and drop

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,23 @@
 import React from "react";
+import {DndProvider} from "react-dnd";
+import Backend from "react-dnd-html5-backend";
 
 class Todo extends React.Component {
   render() {
+
     return (
+      <DndProvider backend={Backend}>
+
+      
       <div
         // Changes class name when item is clicked.
         className={`item${this.props.item.isDone ? "Completed" : ""}`}
         onClick={() => this.props.toggleItem(this.props.item.id)}
+        draggable
       >
         <p>{this.props.item.task}</p>
       </div>
+      </DndProvider>
     );
   };
 }
